38c250c10cc7d9c566b5b6a73da7f9c86cce8d34
Franco Spatocco <fspatocco02@gmail.com>
2023-06-08 23:37:41 -0300

    passRecovery2
    vuelta del pass.Part2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'login', component: LoginComponent },
+{ path: 'forgot-password', component: ForgotPasswordComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
