ea04a15c6b00c80bfbe13d4a0c23fa0148b02be5
Ivan Valeff <ivaleff@gmail.com>
2023-08-12 17:12:37 -0300

    carga materias

--- src/app/components/admin/materias-admin/cargar-materias-dialog-form/cargar-materias-dialog-form.component.ts
+++ src/app/components/admin/materias-admin/cargar-materias-dialog-form/cargar-materias-dialog-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-cargar-materias-dialog-form',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./cargar-materias-dialog-form.component.scss']
 })
 export class CargarMateriasDialogFormComponent {
-
+  cursadas = new FormControl('');
+  aprobadas = new FormControl('');
+  cursadaslist: string[] = ['Mate', 'ingles', 'program'];
+  aprobadaslist = this.cursadaslist;
 }
