5350a14f74568e8d19abfb7025e5ba30d8242f66
Ivan Valeff <ivaleff@gmail.com>
2023-08-17 15:35:58 -0300

    creacion de servicios
    Se crearon las carpetas de servicios

---
+++ src/app/components/home/content-register/cards-data/cards-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-data',
+  templateUrl: './cards-data.component.html',
+  styleUrls: ['./cards-data.component.scss']
+})
+export class CardsDataComponent {
+
+}
