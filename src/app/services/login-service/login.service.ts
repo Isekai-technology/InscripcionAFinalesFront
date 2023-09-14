918422a853c610e798d4693a2c10bf4f17355c41
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-09-14 17:58:23 -0300

    Correxion de url API

--- src/app/services/login-service/login.service.ts
+++ src/app/services/login-service/login.service.ts
@@ -17,7 +17,7 @@ export class LoginService {
   constructor(private http: HttpClient, private router: Router) { }
 
   login(credenciales: CredencialesLogin) : Usuario | Boolean{
-    this.http.post(this.baseUrl + "/login", JSON.stringify(credenciales), {
+    this.http.post(this.baseUrl + "login", JSON.stringify(credenciales), {
       responseType: "text",
       headers: { 'ContentType': 'application/json' }
     }).subscribe((response) => {
