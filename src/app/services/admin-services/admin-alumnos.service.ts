0176bf67cc16b8617d33659a2f29bb9323f355d8
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-09-20 08:06:38 -0300

    Seleccionar un estudiante en la tabla de Alumnos Avances en la peticion de poder ver los datos del alumno en la tarjeta al seleccionarlo en la tabla Usando BehaviourSubject de RxJS en el servicio de alumnos, se puede modificar y acceder a un estudiante desde distintos componentes En la tabla de alumnos, al hacer click, se pasa el ID del estudiante seleccionado al componente El componente ejecuta el metodo clickedRows, buscando el estudiante por Id en la lista Luego, lo guarda en el servicio para que el componente AlumnoCard lo pueda acceder Fix: No estaban importadas ni implementadas las interfaces OnInit y AfterViewInit en el componente AlumnosTable

--- src/app/services/admin-services/admin-alumnos.service.ts
+++ src/app/services/admin-services/admin-alumnos.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Estudiante } from 'src/app/models/Estudiante';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminAlumnosService {
+
+  //tiene que haber una mejor forma de hacer esto
+  private estudianteSource: BehaviorSubject<Estudiante> = new BehaviorSubject<Estudiante>({
+    id:0,
+    nombre:"",
+    apellido:"",
+    dni: "",
+    carrera: "",
+    curso: "",
+    mesasInscriptas: [],
+    usuario: {
+      id: 0, Nombre: "", contra: "", Email: "", ID_Rol: 0
+    },
+    plan: ""
+  });
+
+  estudianteSeleccionado= this.estudianteSource.asObservable();
+
+  seleccionarEstudiante(estudiante: Estudiante){
+    this.estudianteSource.next(estudiante);
+  }
 
   constructor() { }
 }
