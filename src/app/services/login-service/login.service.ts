72660d3e88cc6bb43b34381c23d64f70d15a8ba7
Ezequiel <ezequiel.delafuente93@gmail.com>
2023-09-20 16:52:59 -0300

    Services actualizado

--- src/app/services/login-service/login.service.ts
+++ src/app/services/login-service/login.service.ts
@@ -1,41 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs'; // Importa Observable desde 'rxjs'
 import { environment } from 'src/environments/environment';
-import { Usuario } from 'src/app/models/Usuario';
-import jwtDecode from 'jwt-decode';
-import { CredencialesLogin } from 'src/app/models/Credenciales';
+import { CredencialesLogin } from 'src/app/models/credenciales';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  baseUrl: string= environment.apiUrl + 'UsuariosContro.php';
+  baseUrl: string = environment.apiUrl + 'UsuariosContro.php';
 
   constructor(private http: HttpClient) { }
 
-  login(credenciales: CredencialesLogin){
-    this.http.post(this.baseUrl, JSON.stringify(credenciales), {
+  // Modifica el método login para que devuelva un Observable<any>
+  login(credenciales: CredencialesLogin): Observable<any> {
+    return this.http.post(this.baseUrl, JSON.stringify(credenciales), {
       responseType: 'text',
-    }).subscribe(
-      (response) => {
-        try {
-        let res : Usuario = JSON.parse(response);
-        localStorage.setItem('usuario', res.Nombre);
-        localStorage.setItem('email', res.Email);
-        console.log (res)
-        if (res.ID_Rol == 1)        
-          localStorage.setItem('rol', 'admin');
-        else if(res.ID_Rol == 2)
-          localStorage.setItem('rol', 'usuario');
-        localStorage.setItem('carrera', 'publicidad');
-      } catch (error) {
-        console.log(error); 
-      }
     });
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem('usuario');
     localStorage.removeItem('email');
     localStorage.removeItem('rol');
