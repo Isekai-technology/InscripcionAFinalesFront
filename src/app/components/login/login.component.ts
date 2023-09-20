f67d70e257a4d0d12a37feeb57ff7f08f590d415
Ezequiel <ezequiel.delafuente93@gmail.com>
2023-09-20 16:54:51 -0300

    Login a espera del back

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,54 +1,59 @@
-import { Component, Input } from '@angular/core';
-import { FormControl, FormGroup, Validators,  } from '@angular/forms';
-import { Router } from '@angular/router';
+  import { Component } from '@angular/core';
+  import { FormControl, FormGroup, Validators } from '@angular/forms';
+  import { Router } from '@angular/router';
 
-import { CredencialesLogin } from 'src/app/models/Credenciales';
-import { LoginService } from 'src/app/services/login-service/login.service';
+  import { CredencialesLogin } from 'src/app/models/credenciales';
+  import { LoginService } from 'src/app/services/login-service/login.service';
 
+  @Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+    providers: [LoginService]
+  })
+  export class LoginComponent {
 
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
-  providers: [LoginService]
-})
-export class LoginComponent {
+    constructor(private router: Router, private _loginSer: LoginService) {
+      localStorage.clear();
+    }
 
-  constructor (private router: Router, private _loginSer: LoginService){
-    localStorage.clear();
-  }
+    // Estados
+    failedLogin: boolean = false;
+    notRegistered: boolean = false;
+    registrationOk: boolean = false;
+    usernameAlreadyExists: boolean = false;
 
-  // Estados
-  failedLogin: boolean = false;
-  notRegistered: boolean = false;
-  registrationOk: boolean = false;
-  usernameAlreadyExists: boolean = false;
+    // Formulario
+    loginForm = new FormGroup({
+      username: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+      password: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+    });
 
-  // Formulario
-  loginForm = new FormGroup({
-    username: new FormControl<string>('', {nonNullable: true, validators: Validators.required}),
-    password: new FormControl<string>('',  {nonNullable: true, validators: Validators.required}),
-  });
+    submit() {
+      const credentials: CredencialesLogin = {
+        nombre: this.loginForm.get('username')!.value,
+        contra: this.loginForm.get('password')!.value,
+        tipo: 'login'
+      };
 
-  submit() {
-    const credentials: CredencialesLogin = {
-      nombre: this.loginForm.get('username')!.value,
-      contra: this.loginForm.get('password')!.value,
-      tipo: 'login'
-    };
       
-    this._loginSer.login(credentials);
+      this._loginSer.login(credentials).subscribe(
+        (response: any) => { 
+          const rol = response.role;
+          localStorage.setItem('rol', rol);
 
-    const rol= localStorage.getItem('rol');
-    console.log(rol);
-    if (rol == 'admin') {
-      localStorage.clear();
-      this.router.navigateByUrl('/admin');
-    }
-    else if (rol== 'usuario'){
-      localStorage.clear();
-      this.router.navigateByUrl("/home");
+          if (rol === 'admin') {
+            this.router.navigateByUrl('/admin');
+          } else if (rol === 'usuario') {
+            this.router.navigateByUrl('/home');
+          } 
+            
+          
+        },
+        (error: any) => { 
+          
+          console.error(error);
+        }
+      );
     }
   }
-
-}
