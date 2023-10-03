8add59aa7c8519b0c207b5ea52c5a94d32f97e2a
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-10-03 18:39:32 -0300

    Selector de carreras y planes

--- src/app/components/admin/alumnos-admin/cargar-alumnos/cargar-alumnos.component.ts
+++ src/app/components/admin/alumnos-admin/cargar-alumnos/cargar-alumnos.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { AdminAlumnosService } from 'src/app/services/admin-services/admin-alumnos.service';
 import { AdminUsuariosService } from 'src/app/services/admin-services/admin-usuarios.service';
 
@@ -11,7 +9,19 @@ import { AdminUsuariosService } from 'src/app/services/admin-services/admin-usua
 })
 export class CargarAlumnosComponent {
 
+  carreraSeleccionada: string= "";
+  idCarreraSeleccionada: number = -1;
+
   constructor(private _alumnosService : AdminAlumnosService, private _usuariosService: AdminUsuariosService) {}
 
+  //tiene que haber una mejor forma de hacer esto
+  seleccionarCarrera(){
+    if (this.carreraSeleccionada === "analista")
+      this.idCarreraSeleccionada= 1;
+    else
+      this.idCarreraSeleccionada= 2;
+  }
+
   
+
 }
