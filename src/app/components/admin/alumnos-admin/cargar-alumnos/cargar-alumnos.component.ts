f0dd2313d81156d33c1a1218e3935865479a6bc8
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-10-02 23:19:46 -0300

    Agregado servicios y campo "Plan"

--- src/app/components/admin/alumnos-admin/cargar-alumnos/cargar-alumnos.component.ts
+++ src/app/components/admin/alumnos-admin/cargar-alumnos/cargar-alumnos.component.ts
@@ -1,25 +1,17 @@
 import { Component } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { AdminAlumnosService } from 'src/app/services/admin-services/admin-alumnos.service';
+import { AdminUsuariosService } from 'src/app/services/admin-services/admin-usuarios.service';
+
 @Component({
   selector: 'app-cargar-alumnos',
   templateUrl: './cargar-alumnos.component.html',
   styleUrls: ['./cargar-alumnos.component.scss']
 })
 export class CargarAlumnosComponent {
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Carga de Alumnos', cols: 1, rows: 1 },
-        ];
-      }
 
-      return [
-        { title: 'Carga de Alumnos', cols: 2, rows: 1 },
-      ];
-    })
-  );
+  constructor(private _alumnosService : AdminAlumnosService, private _usuariosService: AdminUsuariosService) {}
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  
 }
