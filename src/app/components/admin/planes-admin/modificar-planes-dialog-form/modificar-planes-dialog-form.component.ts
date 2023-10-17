71c7f8e14cdd629b332a2dc926f117ea53f5a0b4
Ezequiel de la Fuente <133174623+DeLaFuenteEzequiel@users.noreply.github.com>
2023-10-16 22:10:54 -0300

    Admin en proceso
    Planes ok, materias ok, falta eliminar y ajustar servicios

--- src/app/components/admin/planes-admin/modificar-planes-dialog-form/modificar-planes-dialog-form.component.ts
+++ src/app/components/admin/planes-admin/modificar-planes-dialog-form/modificar-planes-dialog-form.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-modificar-planes-dialog-form',
   templateUrl: './modificar-planes-dialog-form.component.html',
   styleUrls: ['./modificar-planes-dialog-form.component.scss']
 })
-export class ModificarPlanesDialogFormComponent {
+export class ModificarPlanesDialogFormComponent implements OnInit {
+  materiasSeleccionadas: string[] = [];
 
+  constructor(@Inject(MAT_DIALOG_DATA) public datos: any) {}
+
+  ngOnInit() {
+    this.materiasSeleccionadas = [...this.datos.materias];
+  }
 }
